8e93c93d6b8fe22d20f4e48e8005cb1bd758d336
KubanovValentin <delo.biss13@gmail.com>
2021-08-14 18:00:42 +0300

    we are starting work on the modal windows of the site

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,7 +16,48 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-// -------------------------------------------------
+// -------------------------------------------------modal
+    $('[data-modal=application], [data-modal=consultation]').on('click', function() {
+        $('.overlay, #application, #consultation').fadeIn('slow');
+    });
 
+    $('.modal__close').on('click', function() {
+        $('.overlay, #application, #consultation, #thanks').fadeOut('slow');
+    });
+
+    
+
+function validateForms(form) {
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите свое имя",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+            },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен адрес почты"
+            }
+        }
+    });
+}
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 
 });
