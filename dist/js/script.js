73e43e87be2ecc3c589a49052e7fc71a043f760f
KubanovValentin <delo.biss13@gmail.com>
2021-08-15 10:43:16 +0300

    add modal on servicesPage

--- dist/js/script.js
+++ dist/js/script.js
@@ -20,6 +20,9 @@ window.addEventListener('DOMContentLoaded', () => {
     $('[data-modal=application]').on('click', function() {
         $('.overlay, #application').fadeIn('slow');
     });
+    $('[href="#openModal"], [href="#openModal1"]').on('click', function() {
+        $('.overlay, #openModal').fadeIn('slow');
+    });
     $('[data-modal=consultation]').on('click', function() {
         $('.overlay, #consultation').fadeIn('slow');
     });
@@ -65,15 +68,17 @@ function validateForms(form) {
 
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
-    console.log(scrollbar);
-    document.querySelector('[href="#openModal"], [href="#openModal1"]').addEventListener('click', function () {
-        document.body.style.overflow = 'hidden';
-        document.querySelector('#openModal, #openModal1').style.marginLeft = scrollbar;
-    });
-    document.querySelector('[href="#close"]').addEventListener('click', function () {
-        document.body.style.overflow = 'visible';
-        document.querySelector('#openModal, #openModal1').style.marginLeft = '0px';
-    });
-  });
+// document.addEventListener("DOMContentLoaded", function () {
+//     var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
+//     console.log(scrollbar);
+//     document.querySelector('[href="#openModal"], [href="#openModal1"]').addEventListener('click', function () {
+//         document.body.style.overflow = 'hidden';
+//         document.querySelector('#openModal, #openModal1').style.marginLeft = scrollbar;
+//         document.body.style.overflow = 'hidden';
+//         document.html.style.marginRight = '17px';
+//     });
+//     document.querySelector('[href="#close"]').addEventListener('click', function () {
+//         document.body.style.overflow = 'visible';
+//         document.querySelector('#openModal, #openModal1').style.marginLeft = '0px';
+//     });
+//   });
